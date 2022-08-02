60afad20916c3abfa61db2f9f80d6690139d70a8
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2022-08-01 22:00:17 -0500

    Add zenbook-specific config

--- .config/i3blocks/battery.sh
+++ .config/i3blocks/battery.sh
@@ -1,14 +1,14 @@
 # Stolen from i3blocks website.
 
 #!/bin/bash
-BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+BAT=$(cat /sys/class/power_supply/BATT/capacity)
 
 # Full and short texts
 echo "Battery: $BAT%"
 echo "BAT: $BAT%"
 
 # Set urgent flag below 5% or use orange below 20%
-if [ $(cat /sys/class/power_supply/BAT0/status) = "Full" ]
+if [ $(cat /sys/class/power_supply/BATT/status) = "Full" ]
 then
     echo "#1CB34B"
 elif [ ${BAT} -le 20]
