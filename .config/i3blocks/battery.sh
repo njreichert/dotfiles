a9870d50dae0765745c3a67188e324535c0db497
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2023-01-04 16:07:28 -0600

    Add config for thinkpad.

--- .config/i3blocks/battery.sh
+++ .config/i3blocks/battery.sh
@@ -1,14 +1,15 @@
 # Stolen from i3blocks website.
 
 #!/bin/bash
-BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+BAT1=$(cat /sys/class/power_supply/BAT0/capacity)
+BAT2=$(cat /sys/class/power_supply/BAT1/capacity)
+
 
 # Full and short texts
-echo "Battery: $BAT%"
-echo "BAT: $BAT%"
+echo "B1: $BAT1% B2: $BAT2%"
 
 # Set urgent flag below 5% or use orange below 20%
-if [ $(cat /sys/class/power_supply/BAT0/status) = "Full" ]
+if [ $(cat /sys/class/power_supply/BATT/status) = "Full" ]
 then
     echo "#1CB34B"
 elif [ ${BAT} -le 20]
