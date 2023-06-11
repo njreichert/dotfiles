0eda7f19a9f5d6994b201023a37e1e2595b1a698
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2023-06-10 23:43:08 -0500

    Add second battery.

--- .config/sway/status_bar.sh
+++ .config/sway/status_bar.sh
@@ -4,7 +4,17 @@
 # Use sway's json status bar, first sending a version prompt,
 # Then starting an endless array.
 
-battery_path=/sys/class/power_supply/BAT0/capacity
+get_battery() {
+    if [ -f $1capacity ]
+    then
+        capacity=$(cat $1capacity)
+        printf "%s%%" $capacity
+    fi
+}
+
+bat_one=/sys/class/power_supply/BAT0/
+bat_two=/sys/class/power_supply/BAT1/
+chg=/sys/class/power_supply/AC/online
 echo "{ \"version\": 1 }"
 echo "["
 echo "[]"
@@ -13,22 +23,30 @@ while :;
 do
     echo ",[
                 {
-                    \"name\": \"battery\",
-                    \"full_text\": \"$(test -f $battery_path && printf "Battery: %s%%" $(cat $battery_path))\"
+                    \"name\": \"chg\",
+                    \"full_text\": \"$([ $(cat $chg | tr -d '\n') -gt 0 ] && echo ⚡)\"
+                },
+                {
+                    \"name\": \"bat0\",
+                    \"full_text\": \"$(get_battery $bat_one)\"
+                },
+                {
+                    \"name\": \"bat1\",
+                    \"full_text\": \"$(get_battery $bat_two)\"
                 },
                 {
                     \"name\": \"brightness\",
-                    \"full_text\": \"Brightness: $(light -G)%\"
+                    \"full_text\": \"Brightness: $(brightnessctl -m | awk -F, '{print $4}')\"
                 },
                 {
                     \"name\": \"volume\",
-                    \"full_text\": \"$(if [ $(pulsemixer --get-mute) = 1 ]; then echo -n "Muted:"; else echo -n "Volume:"; fi) $(pulsemixer --get-volume | sed -e 's/\s.*$//')%\" 
+                    \"full_text\": \"$(if [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ]; then echo -n "Muted:"; else echo -n "Volume:"; fi) $(pactl get-sink-volume @DEFAULT_SINK@ | awk -F/ '{print $2}' | xargs)\" 
                 },
                 {
                     \"name\": \"date_time\",
-                    \"full_text\": \"$(date +%x\ %X)\"
+                    \"full_text\": \"$(date +%x\ %I:%M\ %p)\"
                 }
         ]" # https://unix.stackexchange.com/a/65967 for the above sed prompt.
-    sleep 1
+    sleep 0.1
 done
 
